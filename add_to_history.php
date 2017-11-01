b74e64ab1db06987daa52a170b25565507cdf7ea
Sergio Sanchez <sergio.ssm.97@gmail.com>
2017-11-01 21:29:56 +0000

    se resta el precio del carrito al saldo del usuario

--- add_to_history.php
+++ add_to_history.php
@@ -9,6 +9,10 @@
                 $history = simplexml_load_file($path . "/historial.xml");
                 
                 $array = json_decode($_COOKIE['cart_items_cookie']);
+                
+                $total_price = 0;
+                	
+                //Añadimos las peliculas compradas al historial	
                 foreach ($array as $key => $movie_id) {
                         $catalogo = simplexml_load_file('catalogo.xml');
                 
@@ -16,8 +20,6 @@
                 	    
                 	foreach($movies as $movie){
                 	        
-                	        echo $movie->id;
-                		
                 		$pelicula = $history->addChild('pelicula');
                                 $pelicula->addChild('id', $movie->id);
                                 $pelicula->addChild('titulo', $movie->titulo);
@@ -38,18 +40,31 @@
                                         $actor->addChild('foto', $a->foto);
                                 }
                 
-                                
                                 $history->saveXML($path . "/historial.xml");
                                 
+                                $total_price += floatval($movie->precio);
                 	}
-                	
                 }
                 
+                //Añadimos al final del fichero de datos del usuario su nuevo saldo
+                $dat_file = fopen("usuarios/" . $_SESSION['username'] . "/datos.dat", "r+");
+	        $i = 0;
+	        while(($line = fgets($dat_file)) !== false){
+
+	        	//Última línea del fichero (saldo actual)
+	        	if((feof($dat_file))){
+	        	        fwrite($dat_file, "\n" . ($line - $total_price));
+	        	}
+	        	
+	        	$i++;
+	        }
+                
+                
                 //Eliminamos la cookie para vaciar el carrito
-                unset($_COOKIE['cart_items_cookie']);
-                setcookie('cart_items_cookie', '', time() - 3600, '/'); 
+                //unset($_COOKIE['cart_items_cookie']);
+                //setcookie('cart_items_cookie', '', time() - 3600, '/'); 
             
-                header('Location: cart.php');
+                //header('Location: cart.php');
         
         }
         
